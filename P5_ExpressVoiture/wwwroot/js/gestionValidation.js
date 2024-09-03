332bc78724880435ead5d15a249d1a650836d12a
Core Rose <cr67.rose@gmail.com>
2024-09-03 14:08:24 +0200

    Fix Script Validation and views

--- P5_ExpressVoiture/wwwroot/js/gestionValidation.js
+++ P5_ExpressVoiture/wwwroot/js/gestionValidation.js
@@ -1,4 +1,6 @@
-﻿function confirmDelete(deleteUrl, message) {
+﻿function confirmDelete(event, deleteUrl, message) {
+    event.preventDefault(); // Empêche la soumission automatique du formulaire
+
     if (confirm(message)) {
         window.location.href = deleteUrl;
     }
